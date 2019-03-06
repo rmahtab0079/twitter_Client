39d726241348b4b1319e663b865b02311a3d3d5c
Rageeb Mahtab <>
2019-03-05 19:16:06 -0500

    Made the first part of the Twitter Client App

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Rageeb Mahtab on 3/5/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let myURL = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myURL , success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
+    }
+    
+    
+   
+
+}
